81b11fbc5bc6261102c080f7b0b92675ef506dd9
Mehwish <mehwishgul715@gmail.com>
2024-04-22 22:17:19 +0500

    commit

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,21 +1,21 @@
-  // JavaScript
-  document.addEventListener("DOMContentLoaded", function() {
+// JavaScript delete tags
+document.addEventListener("DOMContentLoaded", function () {
     // Add event listener to confirm delete button
-    document.getElementById('confirmDeleteBtn').addEventListener('click', function() {
-      // Find and remove the parent element of the clicked delete button
-      var parent = document.querySelector('.search-tag');
-      if (parent) {
-        parent.remove();
-      }
-      // Close the modal
-      var modal = bootstrap.Modal.getInstance(document.getElementById('deleteConfirmationModal'));
-      modal.hide();
+    document.getElementById('confirmDeleteBtn').addEventListener('click', function () {
+        // Find and remove the parent element of the clicked delete button
+        var parent = document.querySelector('.search-tag');
+        if (parent) {
+            parent.remove();
+        }
+        // Close the modal
+        var modal = bootstrap.Modal.getInstance(document.getElementById('deleteConfirmationModal'));
+        modal.hide();
     });
-  });
+});
 
 
-   // JavaScript
-   window.addEventListener('DOMContentLoaded', function() {
+// JavaScript remove collapsed on mobile
+window.addEventListener('DOMContentLoaded', function () {
     // Function to check viewport width and toggle 'collapsed' class
     function toggleSidebarCollapse() {
         var sidebar = document.getElementById('sidebar');
